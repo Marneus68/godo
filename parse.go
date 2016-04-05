5b26a1c73405695daf8006b0836e9d94a48e6ec1
Duane Bekaert <duane.bekaert@gmail.com>
2016-04-05 12:48:04 +0200

    More commands are implemented in their basic form

--- parse.go
+++ parse.go
@@ -6,28 +6,38 @@ import (
 	"github.com/Marneus68/godo/config"
 	"log"
 	"os"
+	"strings"
 )
 
 type OptionsFunc func(args []string)
 
 var f = flag.NewFlagSet("standard flags", flag.ContinueOnError)
 
+var defConfigFilePath *string
+
 var defName *string
 var defPort *string
 var defWeb *bool
 var defWebPort *string
+var defTags *string
+var defComm *string
 
-// Defines the possible optional flags for most commands (create, config)
+// Defines the possible optional flags for most commands
+// (create, config, start, restart)
 func SetStandardFlags() {
+	defConfigFilePath = f.String("conf", "", "Path to the config file")
 	defName = f.String("name", "", "The name of the instance")
 	defPort = f.String("port", "", "Port used for communication")
 	defWeb = f.Bool("web", false, "Enables the web interface")
 	defWebPort = f.String("webport", "", "Port of the web interface")
+	defTags = f.String("tags", "", "Comma separated list of flags")
 }
 
 // Defines the possible flags for the Job control mode
 func SetJobFlags() {
-
+	defName = f.String("name", "", "Name of the job")
+	defComm = f.String("command", "", "Command for the job")
+	defTags = f.String("tags", "", "Tags for the job")
 }
 
 var Options = map[string]OptionsFunc{
@@ -36,7 +46,7 @@ var Options = map[string]OptionsFunc{
 		switch {
 		case len(args) > 1:
 			c := config.NewConfig()
-			switch args[1] {
+			switch strings.ToLower(args[1]) {
 			case "master":
 				c.Type = config.Master
 			case "servant":
@@ -66,16 +76,45 @@ var Options = map[string]OptionsFunc{
 				}
 			}
 			fmt.Println(c.ToString())
-		case len(args) == 1:
-			fallthrough
 		default:
-			fmt.Println("Not enough parameters provided for this command.")
-			PrintUsage()
-			os.Exit(1)
+			FlagError()
 		}
 	},
 	"config": func(args []string) {
-		fmt.Println("config")
+		SetStandardFlags()
+		switch {
+		case len(args) > 1:
+			c := config.NewConfig()
+			switch strings.ToLower(args[1]) {
+			case "edit":
+				if len(args) > 2 {
+					if err := f.Parse(args[3:]); err != nil {
+						log.Fatal("Error while parsing command line parameters")
+					}
+					if f.Parsed() {
+						c.Type = config.Master
+						if *defName != "" {
+							c.Name = *defName
+						}
+						if *defPort != "" {
+							c.Port = *defPort
+						}
+						c.Web = *defWeb
+						if *defWebPort != "" {
+							c.WebPort = *defWebPort
+						}
+					} else {
+						fmt.Println("No parameters provided...")
+						fmt.Println("Attempting to open config file with standard text editor")
+						os.Exit(1)
+					}
+				}
+			default:
+			}
+			fmt.Println(c.ToString())
+		default:
+			FlagError()
+		}
 	},
 	"start": func(args []string) {
 		fmt.Println("start")
@@ -90,6 +129,29 @@ var Options = map[string]OptionsFunc{
 		fmt.Println("status")
 	},
 	"job": func(args []string) {
-		fmt.Println("job")
+		SetJobFlags()
+		switch {
+		case len(args) > 1:
+			switch strings.ToLower(args[1]) {
+			case "create":
+				fmt.Println("CREATING NEW JOB")
+			case "delete":
+				fmt.Println("DELETING JOB")
+			case "start":
+				fmt.Println("STARTING JOB")
+			case "edit":
+				fmt.Println("EDITING JOB")
+			default:
+				FlagError()
+			}
+		default:
+			FlagError()
+		}
 	},
+}
+
+func FlagError() {
+	fmt.Println("Not enough parameters provided for this command.")
+	PrintUsage()
+	os.Exit(1)
 }
