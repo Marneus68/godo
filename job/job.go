78425259ad4444cc8a5120f093b90835bd202b12
Duane Bekaert <duane.bekaert@gmail.com>
2016-04-08 22:16:12 +0200

    Reworked job, added runner commands

--- job/job.go
+++ job/job.go
@@ -1,31 +1,42 @@
 package job
 
-// Structure describing a godo job
-//
-// Name : name of the job
-// Tags : job tags expected by this job to be run by a slave or servant instance
-// Command : command executed by the job
+import (
+	"github.com/Marneus68/godo/config"
+	"os"
+)
+
+/*
+Structure describing a godo job
+
+	Name: name of the job
+	Tags: job tags expected by this job to be run by a slave or servant instance
+	Command: command executed by the job
+	Config: pointer to the confguration of the current instace if it's a slave or servant
+*/
 type Job struct {
 	Name    string
 	Tags    []string
 	Command string
+	Config  *config.Config
 }
 
 // Creates a new empty job
-func NewJob() *Job {
+func NewJob(config *Config) *Job {
 	j := new(Job)
 	j.Name = ""
 	j.Tags = make([]string, 0)
 	j.Command = ""
+	j.Config = config
 	return j
 }
 
 // Creates a new job from a file
-func NewJobFromFile(path string) *Job {
+func NewJobFromFile(path string, config *Config) *Job {
 	j := new(Job)
 	j.Name = ""
 	j.Tags = make([]string, 0)
 	j.Command = ""
+	j.Config = config
 	return j
 }
 
@@ -37,4 +48,10 @@ func (job Job) ReadFromFile(path string) {
 // Save job to file
 func (job Job) SaveToFile(path string) {
 
+}
+
+func (job Job) Run() {
+	go func() {
+		////p, err := os.StarProcess(
+	}()
 }
---
+++ runner.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/Marneus68/godo/config"
+)
+
+func Start(config config.Config) {
+
+}
+
+func Restart(config config.Config) {
+
+}
+
+func Stop(config config.Config) {
+
+}
