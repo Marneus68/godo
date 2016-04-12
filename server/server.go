050220db75b03e18ce959918587b3da09f7b33e8
Duane Bekaert <duane.bekaert@gmail.com>
2016-04-12 19:33:45 +0200

    Server can work independently of pidfile

--- server/server.go
+++ server/server.go
@@ -6,16 +6,21 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
+// Start the godo server and writes the pid of the newly created instance
+// if pidfile isn't an empty string
 func Start(con config.Config, pidfile string) {
-	f, err := os.Create(pidfile)
-	if err != nil {
-		log.Fatal("This shouldn't be happening!")
+	if strings.Compare(pidfile, "") != 0 {
+		f, err := os.Create(pidfile)
+		if err != nil {
+			log.Fatal("This shouldn't be happening!")
+		}
+		_, err = f.WriteString(strconv.Itoa(os.Getpid()))
+		if err != nil {
+			log.Fatal("This shouldn't be happening!")
+		}
 	}
 	fmt.Println("Starting godo with pid " + strconv.Itoa(os.Getpid()) + "...")
-	_, err = f.WriteString(strconv.Itoa(os.Getpid()))
-	if err != nil {
-		log.Fatal("This shouldn't be happening!")
-	}
 }
